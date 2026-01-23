431e8c9398c6b94c170a71246c4bdc5b84a07d6e
28nirupama <nirupamamestha28@gmail.com>
2026-01-23 21:58:53 +0530

    Rustfs update

---
+++ rustfs_test.py
@@ -0,0 +1,11 @@
+import boto3
+from botocore.client import Config
+
+s3 = boto3.client(
+ 's3',
+ endpoint_url='https://storage.todos.monster',
+ aws_access_key_id='JwmIA1pVSnTokCjetL3K',
+ aws_secret_access_key='0bcVozMu1Ca84mlB9GdwX6YErIqALPD5ZxtWUgTH',
+ config=Config(signature_version='s3v4'),
+ region_name='us-east-1'
+)
